6b341ec14c90df1f4f2c375b0dbd2e208a07c451
aigardev <garciadomenechaitor@gmail.com>
2025-03-12 17:17:51 +0100

    feat: #8 Delete unused Import

--- app/src/main/java/com/aigardev/rolecenterapp/core/ui/components/AppDrawer.kt
+++ app/src/main/java/com/aigardev/rolecenterapp/core/ui/components/AppDrawer.kt
@@ -26,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope // Importa rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.aigardev.rolecenterapp.R
